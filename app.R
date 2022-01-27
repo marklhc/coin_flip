b8e0124f8fe43092911b0cf6673d2a61b7522068
Hok Chio (Mark) Lai <marklhc@gmail.com>
2022-01-26 21:12:39 -0800

    Update link for coin images

--- app.R
+++ app.R
@@ -38,8 +38,8 @@ server <- function(input, output) {
     v$count <- v$count + 1
   })
   output$img <- renderUI({
-    img_list <- c(head = "https://pixabay.com/get/g66a154a0de64ead1327520e78dbf673ed2c56e89369b6ad5237df8bb4ec6d496ad15b21315da4e7a9539098a0e0013d5dada377b575ff9c39ef6940cc8c90a61799451a1fa050467e8c0237898eaf78c_640.png",
-                  tail = "https://pixabay.com/get/g597292b33e323e222231fb413947f97bf29a00580b35f92d947fed961d774f765287a634e6299615e881788dacbc4a7dc34a90ea05e5e94cc7e8883bcd8354480073aaa5a7bd35d63c7636a8721fe760_640.jpg")
+    img_list <- c(head = "https://www.pngall.com/wp-content/uploads/1/Bitcoin-PNG-Pic.png",
+                  tail = "https://www.pngall.com/wp-content/uploads/1/Bitcoin-PNG.png")
     tags$img(src = img_list[v$outcome[v$count]], width = "100")
   })
   output$out <- renderText({
